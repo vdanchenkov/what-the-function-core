8323788e9685d95182a84c9b5cd74b16cc071eb3
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-06-07 20:01:38 +0400

    Limit permutation to 3 first elements

--- src/argumentCombinations.js
+++ src/argumentCombinations.js
@@ -1,8 +1,10 @@
 import { permutation } from 'js-combinatorics';
-import { range } from 'lodash';
+import range from 'lodash/range';
 
 export default (...args) => {
-  return permutation(range(args.length)).toArray().map(perm => {
-    return { argsOrder: perm, args: perm.map(i => args[i]) };
+  const permutableArgsNumber = 3;
+  return permutation(range(Math.min(permutableArgsNumber, args.length))).toArray().map(perm => {
+    const argsOrder = perm.concat(range(permutableArgsNumber, args.length, 1));
+    return { argsOrder: argsOrder, args: argsOrder.map(i => args[i]) };
   });
 };
--- src/argumentCombinations.test.js
+++ src/argumentCombinations.test.js
@@ -8,4 +8,11 @@ describe('argumentCombinations()', () => {
     expect(result).to.include({ args: ['a', 'c', 'b'], argsOrder: [0, 2, 1] });
     expect(result).to.include({ args: ['c', 'a', 'b'], argsOrder: [2, 0, 1] });
   });
+
+  it('skips permutations for arguments after 4th', () => {
+    const result = argumentCombinations('a', 'b', 'c', 'd');
+    expect(result.length).to.be.eq(6);
+    expect(result).to.include({ args: ['a', 'c', 'b', 'd'], argsOrder: [0, 2, 1, 3] });
+    expect(result).to.include({ args: ['c', 'a', 'b', 'd'], argsOrder: [2, 0, 1, 3] });
+  })
 })
