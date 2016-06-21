a9cf0bafaff24669140abd5651e5ead20d2d83cf
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-06-21 22:41:46 +0400

    add display option to function definition

--- src/cli/prettyPrintFunctions.js
+++ src/cli/prettyPrintFunctions.js
@@ -1,7 +1,11 @@
 import map from 'lodash/map';
 
 const prettyPrintDefinition = definition => {
-  return `${definition.library}.${definition.name}(${definition.argsLabels.join(', ')})`;
+  if (definition.display) {
+    return definition.display.replace(/(\$)(\d+)/g, (match, p1, p2) => definition.argsLabels[p2 - 1])
+  } else {
+    return `${definition.library}.${definition.name}(${definition.argsLabels.join(', ')})`
+  }
 };
 
 const prettyPrint = (results) => {
