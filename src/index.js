0f473faa96a3447c5be684ea21f3e2ab42e9150b
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-06-22 15:32:33 +0400

    snippets

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import argumentCombinations from './argumentCombinations'
 import functions from './functions'
 import findFunction from './findFunction'
 import outcomes from './outcomes'
+import snippets from './snippets'
 
 export {
   argumentCombinations,
   functions,
   findFunction,
-  outcomes
+  outcomes,
+  snippets
 }
